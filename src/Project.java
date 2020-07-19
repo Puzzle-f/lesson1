b489a6d3f57c301524c90432174729d2437b9f46
Puzzle <puzzleuri@mail.ru>
2020-07-19 14:09:22 +0300

    Initial commit

---
+++ src/Project.java
@@ -0,0 +1,11 @@
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Project {
+
+    public static void main(String[] args) {
+        // write your code here
+        
+    }
+}
